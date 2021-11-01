6882880d2db762f93d610be4be16da9234121a01
starpro123 <starpro123@naver.com>
2021-11-01 16:28:01 +0900

    🔧 Xcode 프로젝트 생성 추가로 Main.storyboard 날리기 설정

---
+++ JipJung/JipJung/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JipJung
+//
+//  Created by 윤상진 on 2021/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
+    }
+
+
+}
+
