144e4ffdd9f81c26ac731bf70d34ca9bc9b4982e
jogiking <whrldhks38@gmail.com>
2021-11-04 14:39:13 +0900

    💄 topView, bottomView 내부 위치 잡기

--- JipJung/JipJung/UI/Sound/MusicPlayerViewController.swift
+++ JipJung/JipJung/UI/Sound/MusicPlayerViewController.swift
@@ -6,17 +6,50 @@
 //
 
 import UIKit
+
 import SnapKit
 
 class MusicPlayerViewController: UIViewController {
+    // MARK: - view builder
+    let topView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .purple
+        return view
+    }()
     
-    let topView = UIView()
-    let bottomView = UIView()
+    let bottomView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .cyan
+        return view
+    }()
     
+    let musicDescriptionView: MusicDescriptionView? = {
+        guard let view = Bundle.main.loadNibNamed(
+            MusicDescriptionView.identifier,
+            owner: self,
+            options: nil
+        )?.first as? MusicDescriptionView else {
+            return nil
+        }
+        view.backgroundColor = .green
+        return view
+    }()
+    
+    let maximView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .magenta
+        return view
+    }()
+    
+    let playButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .gray
+        button.setTitle("Play", for: .normal)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.backgroundColor = .white
 
         setNavigationBar()
@@ -24,6 +57,7 @@ class MusicPlayerViewController: UIViewController {
         setBottomView()
     }
     
+    // MARK: - constraint 설정 + addSubView
     func setNavigationBar() {
         navigationItem.leftBarButtonItem = UIBarButtonItem(
             image: UIImage(systemName: "chevron.backward"),
@@ -40,22 +74,40 @@ class MusicPlayerViewController: UIViewController {
     }
     
     func setTopView() {
-        topView.backgroundColor = .purple
         self.view.addSubview(topView)
-        
         topView.snp.makeConstraints { make in
             make.top.leading.trailing.equalToSuperview()
             make.height.equalTo(view.snp.width)
         }
+        
+        guard let musicDescriptionView = musicDescriptionView else {
+            return
+        }
+
+        topView.addSubview(musicDescriptionView)
+        musicDescriptionView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview().inset(12)
+            make.height.equalTo(120)
+        }
     }
     
     func setBottomView() {
-        bottomView.backgroundColor = .cyan
         self.view.addSubview(bottomView)
-        
         bottomView.snp.makeConstraints { make in
             make.leading.trailing.bottom.equalToSuperview()
             make.top.equalTo(topView.snp.bottom)
+        }
+        
+        bottomView.addSubview(playButton)
+        playButton.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide).inset(12)
+            make.height.equalTo(43)
+        }
+        
+        bottomView.addSubview(maximView)
+        maximView.snp.makeConstraints { make in
+            make.leading.top.trailing.equalToSuperview().inset(12)
+            make.bottom.equalTo(playButton.snp.top).offset(-12)
         }
     }
 }
