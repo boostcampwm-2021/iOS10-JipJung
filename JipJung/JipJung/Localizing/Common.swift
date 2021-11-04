f827ab8be48fbe38aa1868061008880a9ed75e3e
이수현 <soohyeon.lee@kakao.com>
2021-11-04 13:44:41 +0900

    💄 UITabBar 디자인 변경

---
+++ JipJung/JipJung/Localizing/Common.swift
@@ -0,0 +1,25 @@
+//
+//  Common.swift
+//  JipJung
+//
+//  Created by Soohyeon Lee on 2021/11/04.
+//
+
+import UIKit
+
+enum TabBarItems {
+    enum Home {
+        static let title = "Home"
+        static let image = UIImage(systemName: "music.note.house")
+    }
+    
+    enum Explore {
+        static let title = "Explore"
+        static let image = UIImage(systemName: "")
+    }
+    
+    enum Me {
+        static let title = "Me"
+        static let image = UIImage(systemName: "")
+    }
+}
