f938a816885258a2aaa6f916851888cdb9f2d901
이수현 <soohyeon.lee@kakao.com>
2021-11-04 11:08:08 +0900

    ✨ LocalFile을 다루는 API 구현

--- JipJung/JipJung/Infra/File/LocalFileManager.swift
+++ JipJung/JipJung/Infra/File/LocalFileManager.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+protocol LocalFileAccessible {
+  func read(_ filePath: String) -> Data?
+  func write(_ data: Data, at filePath: String) -> Bool
+  func delete(_ filePath: String) -> Bool
+}
+
+class LocalFileManager: LocalFileAccessible {
+  static let shared = LocalFileManager()
+  private init() {}
+  
+  private let cachePath = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+  
+  func read(_ filePath: String) -> Data? {
+    let fileURL = cachePath.appendingPathComponent(filePath)
+    return try? Data(contentsOf: fileURL)
+  }
+  
+  func write(_ data: Data, at filePath: String) -> Bool {
+    let fileURL = cachePath.appendingPathComponent(filePath)
+    do {
+      try data.write(to: fileURL)
+    } catch {
+      return false
+    }
+    return true
+  }
+  
+  func delete(_ filePath: String) -> Bool {
+    let fileURL = cachePath.appendingPathComponent(filePath)
+    do {
+      try FileManager.default.removeItem(at: fileURL)
+    } catch {
+      return false
+    }
+    return true
+  }
+}
