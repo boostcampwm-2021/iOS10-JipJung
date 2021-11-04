29f405c975fb43970ae9f5ec8687f9b869f3105a
jogiking <whrldhks38@gmail.com>
2021-11-04 13:27:03 +0900

    💄 MusicDescriptionView 추가

---
+++ JipJung/JipJung/UI/Sound/Views/MusicDescriptionView.swift
@@ -0,0 +1,23 @@
+//
+//  MusicDescriptionView.swift
+//  JipJung
+//
+//  Created by turu on 2021/11/04.
+//
+
+import UIKit
+
+class MusicDescriptionView: UIView {
+    
+    static let identifier = "MusicDescriptionView"
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subTitleLabel: UILabel!
+    @IBOutlet weak var tagStackView: UIStackView!
+    
+//    let titleLabel = UILabel()
+//    let subTitleLabel = UILabel()
+//    let stackView = UIStackView()
+//    let streamingButton = UIButton()
+    
+}
