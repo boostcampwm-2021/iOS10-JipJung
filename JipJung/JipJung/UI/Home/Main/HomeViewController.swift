4c44315528d1e89892d02cd4f06fb79708d5cf06
이수현 <soohyeon.lee@kakao.com>
2021-11-04 16:39:49 +0900

    💄 HomeView의 스크롤 애니메이션 구현

--- JipJung/JipJung/UI/Home/Main/HomeViewController.swift
+++ JipJung/JipJung/UI/Home/Main/HomeViewController.swift
@@ -7,11 +7,76 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+import SnapKit
 
+class HomeViewController: UIViewController {
+    private let mainScrollView = UIScrollView()
+    private let mainScrollContentsView = UIView()
+    private let topView = UIView()
+    private let bottomView = UIView()
+    
+    private let topViewHeight = 120
+    private let bottomViewHeight = 800
+    private let topBottomYGap = 450
+    private let statusHeight = 47
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        // Do any additional setup after loading the view.
+        
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+        
+        configureMainScrollView()
+        configureTopView()
+        configureBottomView()
+    }
+    
+    private func configureMainScrollView() {
+        mainScrollView.delegate = self
+        mainScrollView.backgroundColor = .orange
+        
+        view.addSubview(mainScrollView)
+        mainScrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        mainScrollView.addSubview(mainScrollContentsView)
+        mainScrollContentsView.snp.makeConstraints {
+            $0.centerX.width.equalToSuperview()
+            $0.top.bottom.equalToSuperview()
+        }
+    }
+    
+    private func configureTopView() {
+        topView.backgroundColor = .cyan
+        view.addSubview(topView)
+        topView.snp.makeConstraints {
+            $0.top.equalTo(view.snp.topMargin)
+            $0.left.right.equalToSuperview()
+            $0.height.equalTo(topViewHeight)
+        }
+    }
+    
+    private func configureBottomView() {
+        bottomView.backgroundColor = .red
+        mainScrollContentsView.addSubview(bottomView)
+        bottomView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(topBottomYGap + topViewHeight)
+            $0.left.right.bottom.equalToSuperview()
+            $0.height.equalTo(bottomViewHeight)
+        }
+    }
+}
+
+extension HomeViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let currentContentsOffsetY = scrollView.contentOffset.y
+        let currentTopBottomYGap = topBottomYGap - (Int(currentContentsOffsetY) + statusHeight)
+        topView.snp.updateConstraints {
+            $0.top.equalTo(view.snp.topMargin).offset(min(0, currentTopBottomYGap))
+        }
     }
 }
