b150c00ccce9d37eadc597b8394ea6ec03190e0c
starpro123 <starpro123@naver.com>
2021-11-04 10:53:32 +0900

    ✨ Maxim Entity 추가

---
+++ JipJung/JipJung/Domain/Entities/Maxim.swift
@@ -0,0 +1,15 @@
+//
+//  Maxim.swift
+//  JipJung
+//
+//  Created by 윤상진 on 2021/11/04.
+//
+
+import Foundation
+
+struct Maxim {
+    let id: String
+    let day: Int
+    let thumbURL: String
+    let content: String
+}
