d4e082b7e5b38d515b6e5b3bc69ac629f0d2896f
Kinaan <blackshooter@hanmail.net>
2021-11-05 23:48:27 +0900

    ✨SearchViewController 레이아웃 구현

---
+++ JipJung/JipJung/UI/Explore/Main/SearchViewController.swift
@@ -0,0 +1,141 @@
+//
+//  SearchViewController.swift
+//  JipJung
+//
+//  Created by 오현식 on 2021/11/04.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+class SearchViewController: UIViewController {
+    // MARK: - Subviews
+    
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.delegate = self
+        searchBar.placeholder = "Search entire library"
+        searchBar.layer.cornerRadius = 3
+        searchBar.barTintColor = .black
+        searchBar.searchTextField.textColor = .darkGray
+        
+        return searchBar
+    }()
+    
+    private lazy var cancelButton: UIButton = {
+        let cancelButton = UIButton()
+        cancelButton.setTitle("Cancel", for: .normal)
+        cancelButton.setTitleColor(.darkGray, for: .normal)
+        
+        return cancelButton
+    }()
+    
+    private lazy var searchStackView: UIStackView = {
+        let searchStackView = UIStackView(arrangedSubviews: [searchBar, cancelButton])
+        searchStackView.axis = .horizontal
+        searchStackView.spacing = 6
+        
+        return searchStackView
+    }()
+    
+    private lazy var searchHistoryTableView: UITableView = {
+        let searchHistoryTableView = UITableView()
+        searchHistoryTableView.backgroundColor = .black
+        searchHistoryTableView.delegate = self
+        searchHistoryTableView.dataSource = self
+        searchHistoryTableView.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.cellIdentifier)
+        
+        return searchHistoryTableView
+    }()
+    
+    // MARK: - Private Variables
+    
+    private var disposeBag: DisposeBag = DisposeBag()
+    private var userDefaultStorage: UserDefaultsStorage = UserDefaultsStorage()
+    
+    // MARK: - Lifecycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+        configureUI()
+        bindUI()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.addSubview(searchStackView)
+        searchStackView.snp.makeConstraints {
+            $0.topMargin.equalToSuperview().offset(20)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(50)
+        }
+        
+        view.addSubview(searchHistoryTableView)
+        searchHistoryTableView.snp.makeConstraints {
+            $0.top.equalTo(searchStackView.snp.bottom).offset(20)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    func bindUI() {
+        cancelButton.rx.tap
+            .bind {
+                self.dismiss(animated: true, completion: nil)
+            }
+            .disposed(by: disposeBag)
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func dismissKeyboard() {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let keyword = searchBar.text  else { return }
+        var searchHistory: [String] = []
+        if let data: [String] = userDefaultStorage.load(for: "searchHistory") {
+            searchHistory = data
+        }
+        searchHistory.append(keyword)
+        userDefaultStorage.save(for: "searchHistory", value: searchHistory)
+        searchHistoryTableView.reloadData()
+        
+        dismissKeyboard()
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "History"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let searchHistory: [String] = userDefaultStorage.load(for: "searchHistory") else { return 0 }
+        return searchHistory.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = searchHistoryTableView.dequeueReusableCell(withIdentifier: SearchTableViewCell.cellIdentifier) as? SearchTableViewCell,
+              let searchHistory: [String] = userDefaultStorage.load(for: "searchHistory")
+        else { return UITableViewCell() }
+        
+        cell.searchHistory.text = searchHistory[indexPath.item]
+        return cell
+    }
+}
