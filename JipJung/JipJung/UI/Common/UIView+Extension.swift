ad221da51f343b04a44762d856b38ae754f6623c
starpro123 <starpro123@naver.com>
2021-11-07 17:47:05 +0900

    ✨ MusicCardCollectionViewCell 레이아웃 설정

---
+++ JipJung/JipJung/UI/Common/UIView+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIView+Extension.swift
+//  JipJung
+//
+//  Created by 윤상진 on 2021/11/07.
+//
+
+import UIKit
+
+extension UIView {
+    func makeCircle() {
+        layer.cornerRadius = bounds.height / 2
+    }
+}
