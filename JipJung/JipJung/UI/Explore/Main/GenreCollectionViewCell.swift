381a15b25c5d84f7897ffb9344e93bfd23f29a82
Kinaan <blackshooter@hanmail.net>
2021-11-09 00:10:34 +0900

    ✨GenreCollectionViewCell 구현

---
+++ JipJung/JipJung/UI/Explore/Main/GenreCollectionViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  CategoryCollectionViewCell.swift
+//  JipJung
+//
+//  Created by 오현식 on 2021/11/08.
+//
+
+import UIKit
+import SnapKit
+
+class GenreCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - Static Constants
+    
+    static let id = "GenreCollectionViewCell"
+    
+    // MARK: - Subviews
+    
+    lazy var genreLabel: UILabel = {
+        let genreLabel = UILabel()
+        genreLabel.textColor = .white
+        
+        return genreLabel
+    }()
+    
+    // MARK: - Initializers
+       
+    override init(frame: CGRect) {
+       super.init(frame: frame)
+       configureUI()
+    }
+   
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureUI()
+    }
+    
+    func configureUI() {
+        addSubview(genreLabel)
+        genreLabel.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
