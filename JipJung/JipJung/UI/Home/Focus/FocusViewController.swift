95410277a2acdccb0db79fa8c12062499959cba6
starpro123 <starpro123@naver.com>
2021-11-10 12:33:53 +0900

    ✨ Focus 폴더생성 & FocusViewController.swift 생성하기

---
+++ JipJung/JipJung/UI/Home/Focus/FocusViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FocusViewController.swift
+//  JipJung
+//
+//  Created by 윤상진 on 2021/11/07.
+//
+
+import UIKit
+
+class FocusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+}
