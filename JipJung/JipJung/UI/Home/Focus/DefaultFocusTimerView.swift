f567601fb70c1dedbc6ffc6217665e797ae0b4ee
starpro123 <starpro123@naver.com>
2021-11-10 12:33:53 +0900

    ✨ DefaultFocusTimer 애니메이션 구현

--- JipJung/JipJung/UI/Home/Focus/DefaultFocusTimerView.swift
+++ JipJung/JipJung/UI/Home/Focus/DefaultFocusTimerView.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class DefaultFocusTimerView: UIView, FocusTimerViewable {
+    enum Size {
+        static let circleRadius = 120.0
+        static let circleLine = 4.0
+    }
+    
+    enum Color {
+        static let pulseCircle = UIColor.systemGray.cgColor
+        static let backgroundCircle = UIColor.systemGray.cgColor
+        static let progreeCircle = UIColor.systemGray5.cgColor
+    }
+    
     weak var delegate: FocusTimerViewDelegate?
+    private let timeProgressLayer = CAShapeLayer()
+    private let pulsesLayer = CALayer()
+    private var pulseCreateSupportTimer: Timer?
     
     lazy var timeLabel: UILabel = {
         let label = UILabel()
@@ -32,22 +46,100 @@ class DefaultFocusTimerView: UIView, FocusTimerViewable {
         configureUI()
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        configurePulse()
+        configureBackgroundCircle()
+        configureProgressBar()
+    }
+    
+    private func configureBackgroundCircle() {
+        let backGroundMaskLayer = CAShapeLayer()
+        backGroundMaskLayer.path = UIBezierPath(
+            arcCenter: CGPoint(x: center.x, y: center.y - Size.circleRadius / 2),
+            radius: Size.circleRadius, startAngle: 0,
+            endAngle: 2 * CGFloat.pi,
+            clockwise: true
+        ).cgPath
+        backGroundMaskLayer.fillColor = nil
+        backGroundMaskLayer.strokeColor = UIColor.systemGray.cgColor
+        backGroundMaskLayer.lineWidth = Size.circleLine
+        layer.addSublayer(backGroundMaskLayer)
+    }
+    
+    private func configureProgressBar() {
+        let timeProgressPath = UIBezierPath(
+            arcCenter: CGPoint(x: center.x, y: center.y - Size.circleRadius / 2),
+            radius: Size.circleRadius,
+            startAngle: -CGFloat.pi * 0.5,
+            endAngle: 2 * CGFloat.pi,
+            clockwise: true
+        )
+        timeProgressLayer.fillColor = nil
+        timeProgressLayer.path = timeProgressPath.cgPath
+        timeProgressLayer.strokeColor = Color.progreeCircle
+        timeProgressLayer.lineWidth = 6
+        timeProgressLayer.strokeStart = 0
+        timeProgressLayer.strokeEnd = 0
+        timeProgressLayer.lineCap = .round
+        layer.addSublayer(timeProgressLayer)
+    }
+    
+    private func configurePulse() {
+        layer.addSublayer(pulsesLayer)
+        for _ in 1...4 {
+            let pulseLayer = CAShapeLayer()
+            pulseLayer.path = UIBezierPath(
+                arcCenter: .zero,
+                radius: Size.circleRadius, startAngle: 0,
+                endAngle: 2 * CGFloat.pi,
+                clockwise: true
+            ).cgPath
+            pulseLayer.position = CGPoint(x: center.x, y: center.y - Size.circleRadius / 2)
+            pulseLayer.fillColor = nil
+            pulseLayer.strokeColor = Color.pulseCircle
+            pulseLayer.lineWidth = Size.circleLine
+            pulsesLayer.addSublayer(pulseLayer)
+        }
+    }
+    
     private func configureUI() {
-//        backgroundColor = .systemBackground
         addSubview(timeLabel)
+        
         timeLabel.snp.makeConstraints { make in
             make.width.equalTo(100)
             make.height.equalTo(50)
             make.center.equalToSuperview()
         }
     }
-    
+
     func startFocus(with time: String) {
         timeLabel.text = time
+        var count = 0
+        
+        pulseCreateSupportTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self] _ in
+            if count >= self?.pulsesLayer.sublayers?.count ?? 0 {
+                self?.pulseCreateSupportTimer?.invalidate()
+                return
+            }
+            self?.pulsesLayer.sublayers?[count].add(PulseAnimation(), forKey: nil)
+            count += 1
+        })
+        pulseCreateSupportTimer?.fire()
+        
+        let animation = CABasicAnimation(keyPath: #keyPath(CAShapeLayer.strokeEnd))
+        animation.fromValue = 0
+        animation.toValue = 1
+        animation.duration = 100
+        animation.fillMode = .forwards
+        timeProgressLayer.add(animation, forKey: "key")
     }
     
     func endFocus() {
         timeLabel.text = "종료"
+        timeProgressLayer.removeAllAnimations()
+        pulseCreateSupportTimer?.invalidate()
+        pulsesLayer.sublayers?.forEach({ $0.removeAllAnimations() })
     }
     
     func changeTime(with time: String) {
--- JipJung/JipJung/UI/Home/Focus/FocusViewController.swift
+++ JipJung/JipJung/UI/Home/Focus/FocusViewController.swift
@@ -6,12 +6,131 @@
 //
 
 import UIKit
+import RxSwift
 
 class FocusViewController: UIViewController {
-
+    enum Factory {
+        static func makeDefaultTimer() -> FocusViewController {
+            return FocusViewController(
+                focusTimerView: DefaultFocusTimerView(),
+                focusStartView: DefaultFocusStartView()
+            )
+        }
+    }
+    var focusHeaderView: FocusHeaderView
+    var focusTimerView: FocusTimerViewable
+    var focusStartView: FocusStartViewable
+    
+    init(focusTimerView: DefaultFocusTimerView, focusStartView: DefaultFocusStartView) {
+        self.focusHeaderView = FocusHeaderView()
+        self.focusTimerView = focusTimerView
+        self.focusStartView = focusStartView
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        // Do any additional setup after loading the view.
+        configureUI()
+        configureDelegate()
+        bind()
+    }
+    
+    func configureUI() {
+        view.addSubview(focusHeaderView)
+        focusHeaderView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.height.equalTo(60)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+        
+        view.addSubview(focusTimerView)
+        focusTimerView.snp.makeConstraints { make in
+            make.top.equalTo(focusHeaderView.snp.bottom)
+            make.height.equalTo(view.snp.width)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+        
+        let belowView = UIView()
+        view.addSubview(belowView)
+        belowView.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            make.top.equalTo(focusTimerView.snp.bottom)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+        
+        belowView.addSubview(focusStartView)
+        focusStartView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.height.equalTo(120)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+    }
+    
+    func configureDelegate() {
+        focusStartView.delegate = self
+        focusTimerView.delegate = self
+    }
+    
+    func bind() {
+        sub.subscribe(onNext: { [weak self] isRunning in
+            if isRunning {
+                self?.focusStartView.startFocus()
+                self?.focusTimerView.startFocus(with: "0")
+            } else {
+                self?.focusStartView.endFocus()
+                self?.focusTimerView.endFocus()
+            }
+        }).disposed(by: disposable)
+        
+        isRunningFirst.asObservable()
+          .debug("isRunningFirst") // 로그창에 running true, false 출력
+          .flatMapLatest { isRunning in
+          isRunning ? Observable<Int>
+                  .interval(.seconds(1), scheduler: MainScheduler.init()) : .empty()
+          }
+          .subscribe(onNext: { [weak self] value in
+              guard self?.state == true else {
+                  return
+              }
+              self?.focusTimerView.changeTime(with: "\((self?.time ?? 0) - value)")
+          })
+          .disposed(by: disposable)
+    }
+    
+    var state: Bool = false {
+        didSet {
+            sub.onNext(state)
+        }
+    }
+    var time: Int = 100
+    var sub = PublishSubject<Bool>()
+    let isRunningFirst: BehaviorSubject = BehaviorSubject(value: false)
+    // 2. 함수 생성
+
+    var disposable = DisposeBag()
+}
+
+extension FocusViewController: FocusStarViewDelegate {
+    func startButtonDidClicked() {
+        if time < 0 {
+            time = 100
+        }
+        state.toggle()
+        
+        isRunningFirst.onNext(true)
+    }
+}
+
+extension FocusViewController: FocusTimerViewDelegate {
+    func timeDidSet(with value: Int) {
+        print(value)
     }
 }
