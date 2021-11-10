cc212ec34d1a1d55e100b02974cee62e2a576bc2
Kinaan <blackshooter@hanmail.net>
2021-11-10 21:55:13 +0900

    ✨SaveFocusTime 유즈케이스, FocusRecord 엔터티 구현

---
+++ JipJung/JipJung/UI/Home/Main/SaveFocusTimeUseCase.swift
@@ -0,0 +1,31 @@
+//
+//  SaveFocusTimeUseCase.swift
+//  JipJung
+//
+//  Created by 오현식 on 2021/11/10.
+//
+
+import Foundation
+import RxSwift
+
+protocol SaveFocusTimeUseCaseProtocol {
+    func execute(seconds time: Int) -> Single<Bool>
+}
+
+final class SaveFocusTimeUseCase: SaveFocusTimeUseCaseProtocol {
+    private let focusTimeRepository: FucusTimeRepositoryProtocol
+    
+    init(focusTimeRepository: FucusTimeRepositoryProtocol) {
+        self.focusTimeRepository = focusTimeRepository
+    }
+    
+    func execute(seconds time: Int) -> Single<Bool> {
+        let currentDate = Date()
+        return focusTimeRepository.save(record: FocusRecord(id: UUID().uuidString,
+                                                            focusTime: time,
+                                                            year: currentDate.year,
+                                                            month: currentDate.month,
+                                                            day: currentDate.day,
+                                                            hour: currentDate.hour))
+    }
+}
