9df5bc69080c0b30b2cb0470cde4a8f71cfd6575
SoohyeonLee <soohyeon.lee@kakao.com>
2021-11-11 13:16:24 +0900

    ✨ BasicDataLoad 기능 구현 - 진행 중

---
+++ JipJung/JipJung/Domain/UseCases/BaseDataLoadUseCase.swift
@@ -0,0 +1,30 @@
+//
+//  BaseDataLoadUseCase.swift
+//  JipJung
+//
+//  Created by Soohyeon Lee on 2021/11/11.
+//
+
+import Foundation
+
+class BaseDataLoadUseCase {
+    private let mediaListRepository: MediaListRepositoryProtocol
+    
+    init(mediaListRepository: MediaListRepository) {
+        self.mediaListRepository = mediaListRepository
+    }
+    
+    func isLoaded() -> Bool {
+        return false
+    }
+    
+    func load() {
+        guard let url = Bundle.main.url(forResource: "data", withExtension: "json"),
+              let jsonData = try? JSONSerialization.jsonObject(with: Data(contentsOf: url))
+        else {
+            return
+        }
+        
+        print(jsonData)
+    }
+}
