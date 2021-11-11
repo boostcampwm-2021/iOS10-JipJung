a861dabd2924b2813c803c1acaedf5d2ca5ddd61
SoohyeonLee <soohyeon.lee@kakao.com>
2021-11-11 11:38:29 +0900

    ♻️ Realm 객체의 URL 속성을 FileName 속성으로 변경

--- JipJung/JipJung/Domain/Entities/Media.swift
+++ JipJung/JipJung/Domain/Entities/Media.swift
@@ -13,21 +13,28 @@ class Media: Object {
     @Persisted(primaryKey: true) var id: String
     @Persisted var name: String
     @Persisted var mode: Int
-    @Persisted var thumbnailImageURL: String
-    @Persisted var videoURL: String
-    @Persisted var audioURL: String
+    @Persisted var thumbnailImageFileName: String
+    @Persisted var videoFileName: String
+    @Persisted var audioFileName: String
     
     private override init() {
         super.init()
     }
     
-    convenience init(id: String, name: String, mode: Int, thumbnailImageURL: String, videoURL: String, audioURL: String) {
+    convenience init(
+        id: String,
+        name: String,
+        mode: Int,
+        thumbnailImageFileName: String,
+        videoFileName: String,
+        audioFileName: String
+    ) {
         self.init()
         self.id = id
         self.name = name
         self.mode = mode
-        self.thumbnailImageURL = thumbnailImageURL
-        self.videoURL = videoURL
-        self.audioURL = audioURL
+        self.thumbnailImageFileName = thumbnailImageFileName
+        self.videoFileName = videoFileName
+        self.audioFileName = audioFileName
     }
 }
