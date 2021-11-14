253b1fae7ed50e5d629db49f2974ee4fcae82675
Kinaan <blackshooter@hanmail.net>
2021-11-15 00:08:40 +0900

    ♻️Pomodoro Focus 모드 바인딩 개선, 불필요한 코드 삭제

--- JipJung/JipJung/UI/Home/Focus/PomodoroFocusViewController.swift
+++ JipJung/JipJung/UI/Home/Focus/PomodoroFocusViewController.swift
@@ -13,9 +13,25 @@ import RxRelay
 final class PomodoroFocusViewController: FocusViewController {
     // MARK: - Subviews
     
+    private lazy var timePickerView: UIPickerView = {
+        let timePickerView = UIPickerView()
+        timePickerView.delegate = self
+        timePickerView.dataSource = self
+        
+        return timePickerView
+    }()
+    
+    private lazy var minuteLabel: UILabel = {
+        let minuteLabel = UILabel()
+        minuteLabel.text = "min"
+        minuteLabel.textColor = .systemGray
+        
+        return minuteLabel
+    }()
+    
     private lazy var timeLabel: UILabel = {
         let timeLabel = UILabel()
-        timeLabel.text = "00:00"
+        timeLabel.text = "01:00"
         timeLabel.font = UIFont.boldSystemFont(ofSize: 35)
         timeLabel.textColor = .white
         return timeLabel
@@ -127,6 +143,17 @@ final class PomodoroFocusViewController: FocusViewController {
             $0.centerX.equalToSuperview()
         }
         
+        view.addSubview(timePickerView)
+        timePickerView.snp.makeConstraints {
+            $0.center.equalTo(timeLabel)
+        }
+        
+        view.addSubview(minuteLabel)
+        minuteLabel.snp.makeConstraints {
+            $0.centerY.equalTo(timePickerView)
+            $0.centerX.equalTo(timePickerView.snp.centerX).offset(60)
+        }
+        
         view.addSubview(startButton)
         startButton.snp.makeConstraints {
             $0.top.equalTo(view.snp.centerY).multipliedBy(1.4)
@@ -174,61 +201,71 @@ final class PomodoroFocusViewController: FocusViewController {
     }
     
     private func bindUI() {
+        viewModel?.timerState.bind(onNext: { [weak self] in
+                guard let self = self else { return }
+                switch $0 {
+                case .ready:
+                    self.changeStateToReady()
+                case .running(let isContinue):
+                    self.changeStateToRunning()
+                case .paused:
+                    self.changeStateToPaused()
+                }
+            })
+            .disposed(by: disposeBag)
+        
         startButton.rx.tap
             .bind { [weak self] in
                 guard let self = self else { return }
-                self.viewModel?.startClockTimer()
+                self.viewModel?.changeTimerState(to: .running(isContinue: false))
             }
             .disposed(by: disposeBag)
         
         pauseButton.rx.tap
             .bind { [weak self] in
                 guard let self = self else { return }
-                self.viewModel?.pauseClockTimer()
+                self.viewModel?.changeTimerState(to: .paused)
             }
             .disposed(by: disposeBag)
         
         continueButton.rx.tap
             .bind { [weak self] in
                 guard let self = self else { return }
-                self.viewModel?.startClockTimer()
+                self.viewModel?.changeTimerState(to: .running(isContinue: true))
             }
             .disposed(by: disposeBag)
         
         exitButton.rx.tap
             .bind { [weak self] in
                 guard let self = self else { return }
+                self.viewModel?.changeTimerState(to: .ready)
                 self.viewModel?.resetClockTimer()
+                self.viewModel?.saveFocusRecord()
             }
             .disposed(by: disposeBag)
         
         viewModel?.clockTime
             .bind(onNext: { [weak self] in
-                guard let self = self, $0 > 0 else { return }
-                self.timeLabel.text = $0.digitalClockFormatted
-                self.startPulse(second: $0)
-            })
-            .disposed(by: disposeBag)
-        
-        viewModel?.timerState.bind(onNext: { [weak self] in
-                guard let self = self else { return }
-                switch $0 {
-                case .ready:
-                    self.changeStateToReady()
-                    self.stopTimer()
-                case .running(let isContinue):
-                    self.changeStateToRunning()
-                    isContinue ? self.resumeTimerProgressBar() : self.startTimer()
-                case .paused:
-                    self.changeStateToPaused()
-                    self.pauseTimerProgressBar()
+                guard let self = self, $0 > 0,
+                      let focusTime = self.viewModel?.focusTime
+                else { return }
+                if $0 == focusTime {
+                    self.viewModel?.resetClockTimer()
+                    return
                 }
+                self.timeLabel.text = (focusTime - $0).digitalClockFormatted
+                self.startPulseAnimation(second: $0)
             })
             .disposed(by: disposeBag)
     }
     
     private func changeStateToReady() {
+        timeLabel.text = viewModel?.focusTime.digitalClockFormatted
         pauseButton.isHidden = true
+        timeLabel.isHidden = true
+        timePickerView.isHidden = false
+        minuteLabel.isHidden = false
+        removeAllAnimations()
         
         UIView.animate(withDuration: 0.5) { [weak self] in
             guard let self = self else { return }
@@ -253,6 +290,18 @@ final class PomodoroFocusViewController: FocusViewController {
     
     private func changeStateToRunning() {
         startButton.isHidden = true
+        timeLabel.isHidden = false
+        timePickerView.isHidden = true
+        minuteLabel.isHidden = true
+        viewModel?.startClockTimer()
+        switch viewModel?.timerState.value {
+        case .running(isContinue: true):
+            resumeTimerProgressAnimation()
+        case .running(isContinue: false):
+            startTimeProgressAnimation()
+        default:
+            break
+        }
         
         UIView.animate(withDuration: 0.5) { [weak self] in
             guard let self = self else { return }
@@ -273,13 +322,16 @@ final class PomodoroFocusViewController: FocusViewController {
             self.exitButton.isHidden = true
             self.pauseButton.isHidden = false
         }
-        
-        viewModel?.startWaveAnimationTimer()
     }
     
     private func changeStateToPaused() {
         startButton.isHidden = true
         pauseButton.isHidden = true
+        timeLabel.isHidden = false
+        timePickerView.isHidden = true
+        minuteLabel.isHidden = true
+        viewModel?.pauseClockTimer()
+        pauseTimerProgressAnimation()
 
         UIView.animate(withDuration: 0.5) { [weak self] in
             guard let self = self else { return }
@@ -318,7 +370,7 @@ final class PomodoroFocusViewController: FocusViewController {
         return circleShapeLayer
     }
     
-    private func startTimer() {
+    private func startTimeProgressAnimation() {
         let animation = CABasicAnimation(keyPath: #keyPath(CAShapeLayer.strokeEnd))
         animation.fromValue = 0
         animation.toValue = 1
@@ -328,21 +380,58 @@ final class PomodoroFocusViewController: FocusViewController {
         view.layer.addSublayer(timeProgressLayer)
     }
     
-    private func startPulse(second: Int) {
+    private func startPulseAnimation(second: Int) {
         self.pulseGroupLayer.sublayers?[second % 4].add(PulseAnimation(), forKey: "pulse")
     }
     
-    private func pauseTimerProgressBar() {
+    private func pauseTimerProgressAnimation() {
         timeProgressLayer.pauseLayer()
     }
     
-    private func resumeTimerProgressBar() {
+    private func resumeTimerProgressAnimation() {
         timeProgressLayer.resumeLayer()
     }
     
-    private func stopTimer() {
+    private func removeAllAnimations() {
         timeProgressLayer.removeAllAnimations()
         timeProgressLayer.removeFromSuperlayer()
         pulseGroupLayer.sublayers?.forEach({ $0.removeAllAnimations() })
+    }
+}
+
+extension PomodoroFocusViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let focusTime = (viewModel?.focusTimeList[row] ?? 0) * 60
+        viewModel?.setFocusTime(seconds: focusTime)
+        self.timeLabel.text = focusTime.digitalClockFormatted
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        guard let minuteInfo = viewModel?.focusTimeList[row] else { return UILabel() }
+        timePickerView.subviews.forEach {
+            $0.backgroundColor = .clear
+        }
+        var pickerLabel: UILabel = UILabel()
+        pickerLabel = UILabel()
+        pickerLabel.text = "\(minuteInfo)"
+        pickerLabel.textColor = UIColor.white
+        pickerLabel.font = UIFont.systemFont(ofSize: 35)
+        pickerLabel.textAlignment = .center
+        
+        return pickerLabel
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 80
+    }
+}
+
+extension PomodoroFocusViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return viewModel?.focusTimeList.count ?? 0
     }
 }
