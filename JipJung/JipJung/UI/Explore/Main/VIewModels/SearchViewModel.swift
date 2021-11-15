15e0d984833b70c83e080b80a10039bfb84e9c68
Kinaan <blackshooter@hanmail.net>
2021-11-15 22:09:57 +0900

    ✨SearchHistoryViewModel 구현

---
+++ JipJung/JipJung/UI/Explore/Main/VIewModels/SearchViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  SearchViewModel.swift
+//  JipJung
+//
+//  Created by 오현식 on 2021/11/15.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+
+protocol SearchViewModelInput {
+    func saveSearchKeyword(keyword: String)
+    func loadSearchHistory()
+    func removeSearchHistory(at index: Int)
+}
+
+protocol SearchViewModelOutput {
+    var searchHistory: BehaviorRelay<[String]> { get }
+}
+
+final class SearchViewModel: SearchViewModelInput, SearchViewModelOutput {
+    var searchHistory: BehaviorRelay<[String]> = BehaviorRelay<[String]>(value: [])
+    
+    private var disposeBag: DisposeBag = DisposeBag()
+    
+    private let searchHistoryUseCase: SearchHistoryUseCase
+    
+    init(searchHistoryUseCase: SearchHistoryUseCase) {
+        self.searchHistoryUseCase = searchHistoryUseCase
+    }
+
+    func saveSearchKeyword(keyword: String) {
+        var searchHistory = searchHistoryUseCase.load()
+        searchHistory.append(keyword)
+        searchHistoryUseCase.save(searchHistory: searchHistory)
+        
+        self.searchHistory.accept(searchHistory)
+    }
+    
+    func loadSearchHistory() {
+        let searchHistory = searchHistoryUseCase.load()
+        self.searchHistory.accept(searchHistory)
+    }
+    
+    func removeSearchHistory(at index: Int) {
+        guard self.searchHistory.value.count > index else { return }
+        var searchHistory = self.searchHistory.value
+        searchHistory.remove(at: index)
+        self.searchHistory.accept(searchHistory)
+        searchHistoryUseCase.save(searchHistory: searchHistory)
+    }
+}
