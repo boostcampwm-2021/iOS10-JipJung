d4d0d1919d588596d5bfad7f08a12126418b9dc9
starpro123 <starpro123@naver.com>
2021-11-16 11:57:21 +0900

    ✨ Maxim Entity에 speaker Property추가

--- JipJung/JipJung/Domain/Entities/Maxim.swift
+++ JipJung/JipJung/Domain/Entities/Maxim.swift
@@ -14,16 +14,18 @@ class Maxim: Object, Decodable {
     @Persisted var day: Date
     @Persisted var thumbnailImageFileName: String
     @Persisted var content: String
+    @Persisted var speaker: String
     
     private override init() {
         super.init()
     }
     
-    convenience init(id: String, day: Date, thumbnailImageFileName: String, content: String) {
+    convenience init(id: String, day: Date, thumbnailImageFileName: String, content: String, speaker: String) {
         self.init()
         self.id = id
         self.day = day
         self.thumbnailImageFileName = thumbnailImageFileName
         self.content = content
+        self.speaker = speaker
     }
 }
