efd7f5542218ced19084f548031ac5b31bb60d45
Kinaan <blackshooter@hanmail.net>
2021-11-16 14:14:59 +0900

    ✨String extension으로 isNotEmpty 프로퍼티 정의

---
+++ JipJung/JipJung/UI/Common/Extensions/String+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  String+Extension.swift
+//  JipJung
+//
+//  Created by 오현식 on 2021/11/16.
+//
+
+import Foundation
+
+extension String {
+    var isNotEmpty: Bool {
+        return !self.isEmpty
+    }
+}
