3fdb61c7fef2b488ffb0f999c615a6bfa183e65e
Kinaan <blackshooter@hanmail.net>
2021-11-16 14:16:15 +0900

    ✨ExploreViewModel 구현

--- JipJung/JipJung/UI/Explore/Main/VIewModels/ExploreViewModel.swift
+++ JipJung/JipJung/UI/Explore/Main/VIewModels/ExploreViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
+
+protocol ExploreViewModelInput {
+    func search(tag: String)
+}
+
+protocol ExploreViewModelOutput {
+    var searchResult: BehaviorRelay<[Media]> { get }
+}
+
+final class ExploreViewModel: ExploreViewModelInput, ExploreViewModelOutput {
+    var searchResult: BehaviorRelay<[Media]> = BehaviorRelay<[Media]>(value: [])
+    let souundTagList: [SoundTag] = [SoundTag.all, SoundTag.nature, SoundTag.urban, SoundTag.relax, SoundTag.focus, SoundTag.focus, SoundTag.cafe, SoundTag.cafe, SoundTag.lounge, SoundTag.club]
+    
+    private var disposeBag: DisposeBag = DisposeBag()
+    
+    private let searchMediaUseCase: SearchMediaUseCase
+    
+    init(searchMediaUseCase: SearchMediaUseCase) {
+        self.searchMediaUseCase = searchMediaUseCase
+    }
+    
+    func search(tag: String) {
+        guard tag.isNotEmpty else { return }
+        searchMediaUseCase.searchResult
+            .bind { [weak self] in
+                self?.searchResult.accept($0)
+            }
+            .disposed(by: disposeBag)
+        
+        searchMediaUseCase.search(tag: tag)
+    }
+}
