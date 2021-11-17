c49a744dfd690dea05346054d1cdf7a1cba2f09e
Kinaan <blackshooter@hanmail.net>
2021-11-17 15:39:03 +0900

    ✨FetchMediaUrlUseCase 구현

---
+++ JipJung/JipJung/Domain/UseCases/FetchMediaUrlUseCase.swift
@@ -0,0 +1,23 @@
+//
+//  CheckMusicDownloadedUseCase.swift
+//  JipJung
+//
+//  Created by 오현식 on 2021/11/17.
+//
+
+import Foundation
+
+final class FetchMediaUrlUseCase {
+    private let localFileManager = LocalFileManager.shared
+    private let remoteServiceProvider = RemoteServiceProvider.shared
+    
+    func execute(fileName: String) -> URL? {
+        if let fileURL = BundleManager.shared.findURL(fileNameWithExtension: fileName) {
+            return fileURL
+        }
+        if let fileURL = localFileManager.isExsit(fileName) {
+            return fileURL
+        }
+        return nil
+    }
+}
