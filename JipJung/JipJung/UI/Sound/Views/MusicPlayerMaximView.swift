8f03cfb03b8533038c6e4db08e3e7c94a20b2719
Kinaan <blackshooter@hanmail.net>
2021-11-18 20:08:37 +0900

    💄MusicPlayerMaximView UI 개선

--- JipJung/JipJung/UI/Sound/Views/MusicPlayerMaximView.swift
+++ JipJung/JipJung/UI/Sound/Views/MusicPlayerMaximView.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 import SnapKit
+import SwiftUI
 
 class MusicPlayerMaximView: UIView {
     let maximLabel: UILabel = {
@@ -16,7 +17,7 @@ class MusicPlayerMaximView: UIView {
         At night,
         The moon draws faint tides from ear to ear.
         """
-        label.font = .italicSystemFont(ofSize: 18)
+        label.font = .systemFont(ofSize: 14, weight: .semibold)
         label.numberOfLines = 0
         label.textColor = .white
         
@@ -33,11 +34,11 @@ class MusicPlayerMaximView: UIView {
         return label
     }()
     
-    let personNameLabel: UILabel = {
+    let speakerNameLabel: UILabel = {
         let label = UILabel()
         let words = "- J.M. Coetzee"
         label.text = words
-        label.font = .italicSystemFont(ofSize: 18)
+        label.font = .systemFont(ofSize: 18)
         label.textColor = UIColor(white: 1, alpha: 0.35)
         return label
     }()
@@ -54,7 +55,7 @@ class MusicPlayerMaximView: UIView {
     
     func configure() {
         addSubview(maximLabel)
-        addSubview(personNameLabel)
+        addSubview(speakerNameLabel)
         
         maximLabel.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
@@ -62,7 +63,7 @@ class MusicPlayerMaximView: UIView {
             make.height.equalTo(maximLabel.intrinsicContentSize.height)
         }
         
-        personNameLabel.snp.makeConstraints { make in
+        speakerNameLabel.snp.makeConstraints { make in
             make.trailing.equalToSuperview()
             make.top.equalTo(maximLabel.snp.bottom).offset(32)
         }
