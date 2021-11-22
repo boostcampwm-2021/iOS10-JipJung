da51edaf440f2ab827e92c4170ec736300cd548a
starpro123 <starpro123@naver.com>
2021-11-22 17:12:13 +0900

    ✨ MeViewController 정리

--- JipJung/JipJung/UI/Me/Main/Views/MeViewController.swift
+++ JipJung/JipJung/UI/Me/Main/Views/MeViewController.swift
@@ -15,20 +15,5 @@ class MeViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .cyan
         // Do any additional setup after loading the view.
-        
-        let button = UIButton(type: .system)
-        button.setTitle("음원상세화면", for: .normal)
-        button.addTarget(self, action: #selector(buttonTouched(_:)), for: .touchUpInside)
-        view.addSubview(button)
-        button.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
-    }
-    
-    @objc func buttonTouched(_ sender: UIButton) {
-        print(#function, #line)
-        let vc = MusicPlayerViewController()
-        vc.modalPresentationStyle = .fullScreen
-        present(vc, animated: true, completion: nil)
     }
 }
