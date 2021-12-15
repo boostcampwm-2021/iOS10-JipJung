faf5a66e230da939e48ba147d28767394aa676fb
starpro123 <starpro123@naver.com>
2021-12-16 01:48:18 +0900

    ✨ 위, 아래 스크롤이 서로 따로 동작하도록 분리

--- JipJung/JipJung/UI/Home/Maxim/Maxim+Enums.swift
+++ JipJung/JipJung/UI/Home/Maxim/Maxim+Enums.swift
@@ -7,21 +7,26 @@
 
 import UIKit
 
-enum MaximViewSize {
+enum MaximCollectionViewSize {
     static let nocheHeight = UIApplication.statusBarHeight
     static let headerHeight = CGFloat(50)
+    static let cellSize = UIScreen.main.bounds.size
+    static let lineSpacing = CGFloat(2)
+    static let pageWidth = (cellSize.width + lineSpacing)
 }
 
-enum MaximCalendarHeaderCollectionViewSize {
-    static let width = UIScreen.deviceScreenSize.width
-    static let cellSize = CGRect(
+enum MaximCalendarCollectionViewSize {
+    static let contentWidth = UIScreen.deviceScreenSize.width
+    static let size = CGRect(
         origin: .zero,
         size: CGSize(
-            width: MaximCalendarHeaderCollectionViewSize.width / 14,
-            height: MaximCalendarHeaderCollectionViewSize.width / 14
-            + MaximViewSize.nocheHeight
-            + MaximViewSize.headerHeight * 2
+            width: Self.contentWidth / 14,
+            height: Self.contentWidth / 14
+            + MaximCollectionViewSize.nocheHeight
+            + MaximCollectionViewSize.headerHeight * 2
         )
     )
-    static let cellSpacing = MaximCalendarHeaderCollectionViewSize.width / 14
+    static let cellWidth = Self.contentWidth / 14
+    static let lineSpacing = cellWidth
+    static let pageWidth = (cellWidth + lineSpacing) * 7
 }
