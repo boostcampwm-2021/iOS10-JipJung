9dede9109bffce3ad32fd1e8483641a1d186ef64
starpro123 <starpro123@naver.com>
2021-12-23 22:47:39 +0900

    🔧 RealmTests를 위한 설정

---
+++ JipJung/RealmTests/RealmTests.swift
@@ -0,0 +1,34 @@
+//
+//  RealmTests.swift
+//  RealmTests
+//
+//  Created by 윤상진 on 2021/12/23.
+//
+
+import XCTest
+
+import RealmSwift
+
+class RealmTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
